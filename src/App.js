e36b84dec46450e216accaeaeee3b2e1bf927399
zidniryi <zidnirydwan@gmail.com>
2021-10-17 13:53:44 +0700

    feat(weather-app) create home pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
-import Home from './pages/Home.js/Home';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Detail from './pages/Detail/Detail';
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home.js/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Detail from "./pages/Detail/Detail";
 
 function App() {
-    return (
-        <main>
-            <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="/detail" component={Detail} exact />
-                <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/dashboard" component={Dashboard} />
-
-            </Switch>
-        </main>
-    )
+  return (
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/detail" component={Detail} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card, Col, Container, Row } from "react-bootstrap";
+
+import { converToJson } from "../../helper";
+import Menu from "../../component/Menu/Menu";
+import { bmkgRules } from "../../Constant/bmkgRules";
+import { Link } from "react-router-dom";
+import { imageRules } from "../../Constant/imageRules";
+
+function Home() {
+  const [dataMe, setdata] = useState({});
+
+  /**
+   * Get Data From API BMKG and covert it into JSON
+   */
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-JawaTengah.xml",
+        {
+          "Content-Type": "application/xml; charset=utf-8",
+        }
+      );
+
+      const datares = converToJson(response.data);
+      setdata(datares.data.forecast);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <Menu />
+      <Container>
+        <div style={{ marginTop: "2%" }}>
+          <Row>
+            {dataMe &&
+              dataMe.area?.slice(0, 5)?.map((nameArea, index) => {
+                console.log(nameArea, "Shit", nameArea._attributes.level);
+                return (
+                  <Col md={4} key={index}>
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      to={{
+                        pathname: "/detail",
+                        state: {
+                          nameArea: nameArea,
+                          images: imageRules(nameArea._attributes.description),
+                        },
+                      }}
+                    >
+                      <Card style={{ width: "20rem" }}>
+                        <Card.Img
+                          variant="top"
+                          src={imageRules(nameArea._attributes.description)}
+                        />
+                        <Card.Body>
+                          <Card.Title
+                            style={{ color: "#FFFF", fontWeight: "bold" }}
+                          >
+                            {nameArea._attributes.description}
+                          </Card.Title>
+                          <Card.Text
+                            style={{
+                              color: "#FFFF",
+                              fontWeight: "bold",
+                              fontSize: 11,
+                            }}
+                          >
+                            <ul>
+                              <li>
+                                Koordinat : {nameArea._attributes.coordinate}
+                              </li>
+                              <li>
+                                Cuaca :{" "}
+                                {bmkgRules(
+                                  parseInt(nameArea._attributes.level)
+                                )}
+                              </li>
+                            </ul>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                );
+              })}
+          </Row>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default Home;
